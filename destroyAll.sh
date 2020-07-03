4409ca483cee7eceeb6b620e0131d71379ec80f7
medhyal <67660512+medhyal@users.noreply.github.com>
2020-07-03 15:33:40 -0400

    Update destroyAll.sh

--- destroyAll.sh
+++ destroyAll.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+function prop {
+    grep "${1}" ~/ecomm-aws/envprop.properties|cut -d'=' -f2
+}
+
 cd ~/ecomm-aws
 
 kops delete cluster --name $(terraform output domain-name) --state s3://$(prop s3name) --yes
